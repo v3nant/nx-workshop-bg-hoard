5f94a3e5690aa3c168fcc6193d46ee60796dcfec
Venancio Orozco <venancio@herodevs.com>
2023-02-16 21:31:31 -0600

    lab 6

--- apps/store/src/app/app.routes.ts
+++ apps/store/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'game/:id',
+    loadChildren: () =>
+      import('@bg-hoard/store/feature-game-detail').then(
+        (m) => m.StoreFeatureGameDetailModule
+      ),
+  },
+];
